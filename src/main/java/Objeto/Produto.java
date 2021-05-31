5d99271058de35a43173332b57598e1fa3069718
Stokler <Stokler@DESKTOP-I8JIK2O>
2021-05-31 14:56:47 -0300

    ProdutoCRUD

---
+++ src/main/java/Objeto/Produto.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objeto;
+
+/**
+ *
+ * @author Stokler
+ */
+public class Produto {
+    
+    private int id;
+    private String descocao;
+    private double valor;
+    private int quantidade;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the descocao
+     */
+    public String getDescocao() {
+        return descocao;
+    }
+
+    /**
+     * @param descocao the descocao to set
+     */
+    public void setDescocao(String descocao) {
+        this.descocao = descocao;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
+}
